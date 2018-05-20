5ad1215e0f32eeb55e8e962aff246fb27951c40b
ccmanito <648280021@qq.com>
2018-05-20 13:32:15 +0800

    push

--- src/main.js
+++ src/main.js
@@ -2,14 +2,35 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+//import router from './router'
+import Home from './components/Home'
+import 'bootstrap/dist/css/bootstrap.css'
+//Vue.config.productionTip = false
 
-Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+const routes = [
+{
+  path : '/',
+  components : Home
+},{ 
+  path : '/Home',
+  components : Home
+}];
+
+const router = new VueRouter(
+{
+  routes
+});
 /* eslint-disable no-new */
-new Vue({
+var app  = new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  ...App,
+  //components: { App },
+  //template: '<App/>'
 })
