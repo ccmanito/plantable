30a639a6835fe46ae3dcb0fac0cf3811c741fd02
ccmanito <648280021@qq.com>
2018-05-20 23:35:10 +0800

    push

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,36 @@
 <template>
-  <div id = "wrapper">
+  <div id="wrapper">
     <nav class="navbar navbar-default">
       <div class="container">
         <a class="navbar-brand" href="#">
-           <i class="glyphicon glyphicon-time"></i>
-           计划板
+          <i class="glyphicon glyphicon-time"></i>
+          计划板
         </a>
         <ul class="nav navbar-nav">
-          <li><router-link to="/home"> 首页</router-link></li>
+          <li><router-link to="/home">首页</router-link></li>
           <li><router-link to="/time-entries">计划列表</router-link></li>
         </ul>
       </div>
     </nav>
-    <div class="contarner">
-        <div class="col-sm-3">
-        
-        </div>
-        <div class="col-sm-9">
-        <router-view><router-view>
-        </div>   
+    <div class="container">
+      <div class="col-sm-3">
+        <sidebar></sidebar>
+      </div>
+      <div class="col-sm-9">
+         <router-view></router-view>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Sidebar from './components/Sidebar.vue'
+import SidebarVue from './components/Sidebar.vue';
 export default {
-  name: 'App'
+  name: 'app',
+   components: {
+     'sidebar': Sidebar
+   }
 }
 </script>
- 
+
--- src/main.js
+++ src/main.js
@@ -1,36 +1,31 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// src/main.js
 import Vue from 'vue'
-import App from './App'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
-//import router from './router'
+
+import App from './App'
 import Home from './components/Home'
 import 'bootstrap/dist/css/bootstrap.css'
-//Vue.config.productionTip = false
-
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
 
-const routes = [
-{
+const routes = [{
   path : '/',
-  components : Home
-},{ 
-  path : '/Home',
-  components : Home
+  component : Home
+},{
+  path : '/home',
+  component : Home
 }];
 
-const router = new VueRouter(
-{
+const router = new VueRouter({
   routes
 });
+
 /* eslint-disable no-new */
-var app  = new Vue({
+// 实例化我们的Vue
+var app = new Vue({
   el: '#app',
   router,
   ...App,
-  //components: { App },
-  //template: '<App/>'
-})
+});
